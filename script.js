fdc55f431e1f5d9c10f89bbd94114b9227cf1b2d
Cigi <cigi@mirrorlessmind.com>
2021-03-06 07:08:52 -0600

    Initial Variable Set

---
+++ script.js
@@ -0,0 +1,8 @@
+var score = 0;
+var questionIndex = 0;
+ 
+// Declared variables
+var currentTime = document.querySelector("#currentTime");
+var timer = document.querySelector("#startTime");
+var questionsDiv = document.querySelector("#questionsDiv");
+var wrapper = document.querySelector("#wrapper");
